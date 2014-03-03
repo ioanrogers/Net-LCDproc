da786fc92c409159fc3e0dba93595a83fb172895
Ioan Rogers <ioanr@cpan.org>
2014-03-03 05:43:24 -0800

    Cleaned up old test code

--- lib/Net/LCDproc/Role/Widget.pm
+++ lib/Net/LCDproc/Role/Widget.pm
@@ -3,12 +3,7 @@ package Net::LCDproc::Role::Widget;
 use Moo::Role;
 
 sub _set_attr {
-    my ($self, $new_val) = @_;
-
-    #    $log->debugf('Setting %s: "%s"', $attr, $new_val) if $log->is_debug;
-
-    $self->changed(1);
-
+    $_[0]->changed(1);
     return 1;
 }
 
